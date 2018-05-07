722815935a0d67e396be70ddf713225dce2b825a
Diego Machado <diegomachado.designer@gmail.com>
2018-05-06 23:14:59 -0300

    Web basico funcionando

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname+'/views/partials');
+
+//Definindo variavel global para o HBS
+hbs.registerHelper('anoAtual', () => {
+  return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.set('view engine', 'hbs');
+
+//o next espera a função next() para continuar
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}:${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Não foi possivel registrar este log');
+    }
+  });
+  next();
+});
+
+app.use ((req, res, next) => {
+  res.render('maintenance.hbs');
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    tituloPagina: 'Sobre',
+    titulo: 'Bem Vindo',
+    welcomeMessage: 'Chupa Sociedade',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs',{
+    tituloPagina: 'Sobre',
+    titulo: 'Sobre este Site',
+    texto: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000);
